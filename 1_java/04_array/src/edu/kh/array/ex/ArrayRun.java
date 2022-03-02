e52f6aedcb06f01e8804969aed50dc1d7625479e
baek-dh <baekdh@iei.or.kr>
2022-03-02 12:49:27 +0900

    20220302 배열 수업 내용 추가

--- 1_java/04_array/src/edu/kh/array/ex/ArrayRun.java
+++ 1_java/04_array/src/edu/kh/array/ex/ArrayRun.java
@@ -6,6 +6,13 @@ public class ArrayRun {
 		ArrayExample1 arrayEx1 = new ArrayExample1();
 		
 //		arrayEx1.ex1();
-		arrayEx1.ex2();
+//		arrayEx1.ex2();
+//		arrayEx1.ex3();
+//		arrayEx1.ex4();
+//		arrayEx1.ex5();
+//		arrayEx1.ex6();
+//		arrayEx1.ex7();
+//		arrayEx1.ex8();
+		arrayEx1.ex9();
 	}
 }
