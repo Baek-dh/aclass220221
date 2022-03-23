c1f2e6b684443020584204f32b5f6aaf65e7d010
BDH <baekdh@iei.or.kr>
2022-03-23 09:07:27 +0900

    20220323_함수 연습문제

--- 2_db/02_함수.sql
+++ 2_db/02_함수.sql
@@ -261,32 +261,62 @@ FROM EMPLOYEE;
 -- 단, 입사일-오늘의 별칭은 "근무일수1", 
 -- 오늘-입사일의 별칭은 "근무일수2"로 하고
 -- 모두 정수(내림)처리, 양수가 되도록 처리
+SELECT EMP_NAME 사원명,
+    FLOOR( ABS( HIRE_DATE - SYSDATE ) ) "근무일수1",
+    FLOOR( ABS( SYSDATE - HIRE_DATE ) ) "근무일수2"
+FROM EMPLOYEE;
 
 
 -- EMPLOYEE 테이블에서 사번이 홀수인 직원들의 정보 모두 조회
+SELECT  * FROM EMPLOYEE
+-- WHERE SUBSTR(EMP_ID, -1,1) IN('1','3','5','7','9');
+WHERE MOD(EMP_ID, 2) = 1;
 
 
 -- EMPLOYEE 테이블에서 근무 년수가 20년 이상인 직원 정보 조회
+SELECT * FROM EMPLOYEE
+WHERE (EXTRACT(YEAR FROM SYSDATE) - EXTRACT(YEAR FROM HIRE_DATE)) >= 20;
+
 
 -- EMPLOYEE 테이블에서
 -- 직원명과 주민번호를 조회
 -- 단, 주민번호 9번째 자리부터 끝까지는 '*'문자로 채움
 -- 예 : 홍길동 771120-1******
+SELECT EMP_NAME, SUBSTR(EMP_NO, 1, 8) || '******'
+FROM EMPLOYEE;
 
 
 -- EMPLOYEE 테이블에서
 -- 직원명, 직급코드, 연봉(원) 조회
--- 단, 연봉은 보너스가 적용된 1년치 급여 + ￦57,000,000 으로 표시 )
+-- 단, 연봉은 보너스가 적용된 1년치 급여 + ￦57,000,000 으로 표시 
+SELECT EMP_NAME, JOB_CODE, 
+    -- (급여 + (급여 * 보너스) )* 12
+    TO_CHAR((SALARY + (SALARY * NVL(BONUS, 0)))*12, 'L999,999,999') AS "연봉(원)"
+FROM EMPLOYEE;
+
+
+SELECT EMP_NAME, JOB_CODE, 
+    -- 급여 * (1 + 보너스) * 12
+    TO_CHAR( SALARY * (1 +NVL(BONUS,0)) *12 , 'L999,999,999') AS "연봉(원)"
+FROM EMPLOYEE;
 
 
 -- EMPLOYEE 테이블에서
 -- 부서코드가 D5, D9인 직원들 중에서 2004년도에 입사한 직원의 
 -- 사번 사원명 부서코드 입사일 조회
+SELECT EMP_ID, EMP_NAME, DEPT_CODE, HIRE_DATE
+FROM EMPLOYEE
+WHERE DEPT_CODE IN ('D5', 'D9')
+AND EXTRACT(YEAR FROM HIRE_DATE) = 2004;
 
 
 -- EMPLOYEE 테이블에서
 -- 직원명, 입사일, 입사한 달의 근무일수 조회
 -- 단, 입사한 날도 근무일수에 포함해서 +1 할 것
+SELECT EMP_NAME, HIRE_DATE, 
+    LAST_DAY(HIRE_DATE) - HIRE_DATE +1 AS "입사한 달의 근무 일수"
+FROM EMPLOYEE;
+
 
 
 -- EMPLOYEE 테이블에서
@@ -295,3 +325,19 @@ FROM EMPLOYEE;
 -- ㅇㅇ년 ㅇㅇ월 ㅇㅇ일로 출력되게 함.
 -- 나이는 주민번호에서 추출해서 날짜데이터로 변환한 다음, 계산.
 -- (년도만을 이용한 나이 구하기,   만 나이 구하기 둘다 시도해보세요!)
+
+-- 년도만을 이용하여 나이계산1
+SELECT EMP_NAME, DEPT_CODE,
+    SUBSTR(EMP_NO, 1, 2) || '년 ' ||
+    SUBSTR(EMP_NO, 3, 2) || '월 ' || 
+    SUBSTR(EMP_NO, 5, 2) ||'일 ' 생년월일,
+    EXTRACT(YEAR FROM SYSDATE) -
+    EXTRACT(YEAR FROM TO_DATE(SUBSTR(EMP_NO, 1, 6), 'RRMMDD')) "나이"
+FROM EMPLOYEE;
+
+-- 년도만을 이용하여 나이계산2
+SELECT EMP_NAME, DEPT_CODE,
+    TO_CHAR( TO_DATE(SUBSTR(EMP_NO,1,6), 'RRMMDD'), 'RR"년" MM"월" DD"일"') 생년월일,
+    EXTRACT(YEAR FROM SYSDATE) -
+    EXTRACT(YEAR FROM TO_DATE(SUBSTR(EMP_NO,1,6), 'RRMMDD')) "나이"
+FROM EMPLOYEE;
