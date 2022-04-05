28cd43eed40d5421a0e9036d9a7ce05d27923b35
baek-dh <baekdh@iei.or.kr>
2022-04-05 17:37:15 +0900

    20220405_DB완료, member_board (JDBC프로젝트) 세팅1

--- 2_db/10_VIEW,SEQUENCE,INDEX.sql
+++ 2_db/10_VIEW,SEQUENCE,INDEX.sql
@@ -132,6 +132,213 @@ CREATE OR REPLACE VIEW V_DCOPY2
 AS SELECT DEPT_ID, LOCATION_ID FROM DEPARTMENT
 WITH READ ONLY; -- 읽기 전용의 VIEW 생성(DML)
 
+INSERT INTO V_DCOPY2 VALUES ('DD', 'L1');
+-- ORA-42399: 읽기 전용 뷰에서는 DML 작업을 수행할 수 없습니다.
+
+
+--------------------------------------------------------------------------------
+
+/* SEQUENCE(순서, 연속)
+ - 순차적 번호 자동 발생기 역할의 객체
+ EX) 1 2 3 4 5 6 7 8 9 .....
+ 
+ *** SEQUENCE를 사용하는 방법 : PRIMARY KEY 컬럼에 사용될 값을 생성하는 용도로 사용 ***
+
+
+  [작성법]
+  CREATE SEQUENCE 시퀀스이름
+  [STRAT WITH 숫자] -- 처음 발생시킬 시작값 지정, 생략하면 자동 1이 기본
+  [INCREMENT BY 숫자] -- 다음 값에 대한 증가치, 생략하면 자동 1이 기본
+  [MAXVALUE 숫자 | NOMAXVALUE] -- 발생시킬 최대값 지정 (10의 27승 -1)
+  [MINVALUE 숫자 | NOMINVALUE] -- 최소값 지정 (-10의 26승)
+  [CYCLE | NOCYCLE] -- 값 순환 여부 지정
+  [CACHE 바이트크기 | NOCACHE] -- 캐쉬메모리 기본값은 20바이트, 최소값은 2바이트
+
+-- 시퀀스의 캐시 메모리는 할당된 크기만큼 미리 다음 값들을 생성해 저장해둠
+-- --> 시퀀스 호출 시 미리 저장되어진 값들을 가져와 반환하므로 
+--     매번 시퀀스를 생성해서 반환하는 것보다 DB속도가 향상됨.
+
+
+    ** 시퀀스 사용 방법 **
+    
+    1) 시퀀스명.NEXTVAL : 다음 시퀀스 번호를 얻어옴. (INCREMENT BY만큼 증가된 값)
+                          단, 시퀀스 생성 후 첫 호출인 경우 START WITH의 값을 얻어옴.
+    
+    2) 시퀀스명.CURRVAL : 현재 시퀀스 번호 얻어옴.
+                          단, 시퀀스 생성 후 NEXTVAL 호출 없이 CURRVAL를 호출하면 오류 발생.
+    
+
+*/
+
+-- 테이블 복사
+CREATE TABLE EMPLOYEE_COPY4
+AS SELECT EMP_ID, EMP_NAME FROM EMPLOYEE;
+
+-- EMPLOYEE_COPY4의 EMP_ID 컬럼에 PK 제약조건 추가
+ALTER TABLE EMPLOYEE_COPY4
+ADD PRIMARY KEY(EMP_ID);
+
+SELECT * FROM EMPLOYEE_COPY4;
+
+-- 223 부터 시작하여 5씩 증가하는 시퀀스 생성
+CREATE SEQUENCE SEQ_EMP_ID
+START WITH 223   -- (빨간줄 나와도 정상 수행, 디벨로퍼 오류)
+INCREMENT BY 5;
+-- Sequence SEQ_EMP_ID이(가) 생성되었습니다.
+
+-- NEXTVAL 호출 없이 CURRVAL 호출을 하면 발생하는 오류 확인
+SELECT SEQ_EMP_ID.CURRVAL FROM DUAL;
+-- ORA-08002: 시퀀스 SEQ_EMP_ID.CURRVAL은 이 세션에서는 정의 되어 있지 않습니다
+
+
+-- NEXTVAL 호출1
+SELECT SEQ_EMP_ID.NEXTVAL FROM DUAL; -- 최초 호출 == START WITH 값 조회
+-- 223
+
+-- NEXTVAL 호출2
+SELECT SEQ_EMP_ID.NEXTVAL FROM DUAL; -- INCREMENT BY 만큼 증가한 값 조회
+-- 228
+
+-- CURRVAL 호출
+SELECT SEQ_EMP_ID.CURRVAL FROM DUAL; -- NEXTVAL 호출 후 CURRVAL 호출 시 오류 없음
+-- 228
+
+-- 실제 사용
+SELECT * FROM EMPLOYEE_COPY4;
+
+INSERT INTO EMPLOYEE_COPY4 VALUES( SEQ_EMP_ID.NEXTVAL , '홍길동' );
+INSERT INTO EMPLOYEE_COPY4 VALUES( SEQ_EMP_ID.NEXTVAL , '고길동' );
+INSERT INTO EMPLOYEE_COPY4 VALUES( SEQ_EMP_ID.NEXTVAL , '장길산' );
+
+SELECT * FROM EMPLOYEE_COPY4;
+
+SELECT SEQ_EMP_ID.CURRVAL FROM DUAL;
+
+-- 시퀀스는 롤백을 수행한다고 해서 돌아가지 않음.
+--> 계속 증가된 상태를 유지
+ROLLBACK;
+SELECT * FROM EMPLOYEE_COPY4;
+SELECT SEQ_EMP_ID.CURRVAL FROM DUAL;
+
+
+----------------------------
+
+-- SEQUENCE 변경(ALTER)
+/*
+  [작성법]
+  ALTER SEQUENCE 시퀀스이름
+  [INCREMENT BY 숫자] -- 다음 값에 대한 증가치, 생략하면 자동 1이 기본
+  [MAXVALUE 숫자 | NOMAXVALUE] -- 발생시킬 최대값 지정 (10의 27승 -1)
+  [MINVALUE 숫자 | NOMINVALUE] -- 최소값 지정 (-10의 26승)
+  [CYCLE | NOCYCLE] -- 값 순환 여부 지정
+  [CACHE 바이트크기 | NOCACHE] -- 캐쉬메모리 기본값은 20바이트, 최소값은 2바이트
+  
+  --> 시퀀스 변경 시 시작 번호는 변경할 수 없음.
+  
+  * 시퀀스를 잘못 다루어서 번호가 공백이 있어서 다시 처음부터 시작하고 싶은 경우
+  --> 시퀀스 삭제 후 재생성
+*/
+
+-- SEQ_EMP_ID 의 증가값을 5 -> 1로 변경
+ALTER SEQUENCE SEQ_EMP_ID
+INCREMENT BY 1; 
+-- Sequence SEQ_EMP_ID이(가) 변경되었습니다.
+
+SELECT SEQ_EMP_ID.NEXTVAL FROM DUAL;
+
+-- SEQUENCE 삭제
+DROP SEQUENCE SEQ_EMP_ID;
+
+-- VIEW 삭제
+DROP VIEW V_DCOPY2;
+
+
+--------------------------------------------------------------------------------
+
+/*  INDEX(색인)
+    
+- SQL 명령문 중 SELECT의 처리 속도를 향상 시키기 위해 
+  컬럼에 대해서 생성하는 객체
+
+- 인덱스 내부 구조는 B* 트리 형식으로 되어있음.
+
+
+* 인덱스 장점
+ - 이진 트리 형식으로 구성되어 있어 자동 정렬 및 검색 속도가 빠름.
+ - 조회 시 전체 테이블 내용을 조회하는 것이 아닌
+   인덱스가 지정된 컬럼만을 이용해서 조회하기 때문에
+   시스템 부하가 낮아져 전체적인 성능이 향상된다.
+
+* 인덱스 단점
+ - 데이터 변경(INSERT, UPDATE, DELETE) 작업 빈번한 경우 오히려 성능이 저하되는 문제가 발생.
+ - 인덱스도 하나의 객체이다보니 이를 저장하기 위한 별도 공간.
+ - 인덱스 생성 시간이 필요 
+
+
+
+  --인덱스 생성 방법--
+
+    [작성법]
+    CREATE [UNIQUE] INDEX 인덱스명
+    ON 테이블명 (컬럼명, 컬럼명, ... | 함수명, 함수계산식);
+    
+    -- 인덱스가 자동으로 생성되는 경우
+    --> PK 또는 UNIQUE 제약조건이 설정되는 경우
+*/
+
+SELECT * FROM EMPLOYEE_COPY4;
+
+-- EMPLOYEE_COPY4 테이블의 EMP_NAME 컬럼에 INDEX 생성
+CREATE INDEX ECOPY4_NAME_IDX
+ON EMPLOYEE_COPY4(EMP_NAME);
+-- Index ECOPY4_NAME_IDX이(가) 생성되었습니다.
+
+SELECT * FROM EMPLOYEE_COPY4;
+--> 인덱스를 사용하지 않은 검색
+
+
+-- *** 인덱스를 이용한 조회(검색) 방법 ***
+--> WHERE절에 인덱스가 추가된 컬럼이 언급되면 자동으로 INDEX가 활용됨.
+SELECT * FROM EMPLOYEE_COPY4
+WHERE EMP_NAME != '0';
+--> 데이터가 너무 적어서 차이가 거의 없음 ....
+
+
+-- 인덱스 확인용 테이블 생성
+CREATE TABLE TB_IDX_TEST(
+    TEST_NO NUMBER PRIMARY KEY, -- 자동으로 인덱스가 생성됨.
+    TEST_ID VARCHAR2(20) NOT NULL
+);
+
+-- TB_IDX_TEST 테이블에 샘플데이터 100만개 삽입 (PL/SQL 사용)
+BEGIN
+    FOR I IN 1..1000000
+    LOOP
+        INSERT INTO TB_IDX_TEST VALUES( I , 'TEST' || I );
+    END LOOP;
+    
+    COMMIT;
+END;
+/
+
+SELECT COUNT(*) FROM TB_IDX_TEST;
+
+-- 인덱스 사용 X
+SELECT * FROM TB_IDX_TEST
+WHERE TEST_ID = 'TEST500000'; -- 0.982
+
+-- 인덱스 사용 O
+SELECT * FROM TB_IDX_TEST
+WHERE TEST_NO = 500000; -- 0.005
+
+-- 테스트용 테이블 삭제
+DROP TABLE TB_IDX_TEST;
+
+-- 인덱스 삭제
+DROP INDEX ECOPY4_NAME_IDX;
+
+SELECT * FROM EMPLOYEE;
+
 
 
 
